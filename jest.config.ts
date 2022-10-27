729dc787241218dec8edea34f8f532841e413f1d
Ndav07 <nikollasdavidor@hotmail.com>
2022-10-27 10:43:39 -0300

    chore: add jest to lint staged

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  root: ['<rootDir>/src'],
-  bail: true,
-  clearMocks: true,
-  coverageProvider: "v8",
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: [ "**/*.spec.ts" ],
-};
