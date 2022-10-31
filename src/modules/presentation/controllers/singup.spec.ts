be7a53348e53472df6635dccbfab14dcd6de22c4
Ndav07 <nikollasdavidor@hotmail.com>
2022-10-31 09:23:39 -0300

    teste

--- src/modules/presentation/controllers/singup.spec.ts
+++ src/modules/presentation/controllers/singup.spec.ts
@@ -1,46 +1,46 @@
 import { SignUpController } from "@modules/presentation/controllers/singup"
 import { MissingParamError } from "../erros/missing-param-error"
 
-describe('SignUp Controller', () => {
-  it('should return 400 if no name is provide', () => {
+describe("SignUp Controller", () => {
+  it("should return 400 if no name is provide", () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        email: 'any_email@email.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
+        email: "any_email@email.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('name'))
+    expect(httpResponse.body).toEqual(new MissingParamError("name"))
   })
 
-  it('should return 400 if no email is provide', () => {
+  it("should return 400 if no email is provide", () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'any_name',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+    expect(httpResponse.body).toEqual(new MissingParamError("email"))
   })
 
-  it('should return 400 if no password is provide', () => {
+  it("should return 400 if no password is provide", () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'any_name',
-        email: 'any_email@email.com',
-        passwordConfirmation: 'any_password'
-      }
+        name: "any_name",
+        email: "any_email@email.com",
+        passwordConfirmation: "any_password",
+      },
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+    expect(httpResponse.body).toEqual(new MissingParamError("password"))
   })
-});
+})
