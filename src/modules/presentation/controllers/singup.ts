c000c10d706b121a842475daa80b85739934be66
Ndav07 <nikollasdavidor@hotmail.com>
2022-10-31 08:49:35 -0300

    chore: add prettier

--- src/modules/presentation/controllers/singup.ts
+++ src/modules/presentation/controllers/singup.ts
@@ -1,12 +1,12 @@
 import { HttpRequest, HttpResponse } from "./protocols/http"
 import { MissingParamError } from "../erros/missing-param-error"
-import { badRequest } from "../helpers/http-helper";
+import { badRequest } from "../helpers/http-helper"
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password' ]
-    for(const field of requiredFields) {
-      if(!httpRequest.body[field]) {
+    const requiredFields = ["name", "email", "password"]
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
     }
